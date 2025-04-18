b86d3b70670e39c5563fdcbca9d753329ac97153
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-04-18 09:58:45 -0400

    complete DeepDSC

--- DeepDSC/run_new_cell_gdsc1.sh
+++ DeepDSC/run_new_cell_gdsc1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name='G1C DeepDSC'
-#SBATCH --mem=64G
-#SBATCH --partition=a100-4 
-#SBATCH --gres=gpu:a100:1
+#SBATCH --partition gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --nodes=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=inoue019@umn.edu
 #SBATCH --time=24:00:00
