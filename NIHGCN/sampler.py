fccabfbb609e06f312417e9fb3bd96fb64450af4
inoue0426 <8393063+inoue0426@users.noreply.github.com>
2025-05-08 15:09:27 -0400

    comment out random sampler

--- NIHGCN/sampler.py
+++ NIHGCN/sampler.py
@@ -33,83 +33,79 @@ class BalancedSampler(object):
 
 
 
-class RandomSampler(object):
-    # 元の辺をサンプリング
-    # サンプリング後にテストセットと訓練セットを生成
-    # 処理後の訓練セットをtorch.tensor形式に変換
+# class RandomSampler(object):
+#     # 元の辺をサンプリング
+#     # サンプリング後にテストセットと訓練セットを生成
+#     # 処理後の訓練セットをtorch.tensor形式に変換
 
-    def __init__(self, adj_mat_original, train_index, test_index, null_mask, seed):
-        # Initialize basic attributes
-        self.seed = seed
-        self.set_seed()
+#     def __init__(self, adj_mat_original, train_index, test_index, null_mask, seed):
+#         # Initialize basic attributes
+#         self.seed = seed
+#         self.set_seed()
 
-        self.adj_mat = to_coo_matrix(adj_mat_original)
-        self.train_index = train_index
-        self.test_index = test_index
-        self.null_mask = null_mask
+#         self.adj_mat = to_coo_matrix(adj_mat_original)
+#         self.train_index = train_index
+#         self.test_index = test_index
+#         self.null_mask = null_mask
 
-        self.train_pos = self.sample(train_index)
-        self.test_pos = self.sample(test_index)
-        self.train_neg, self.test_neg = self.sample_negative()
-        self.train_mask = mask(self.train_pos, self.train_neg, dtype=int)
-        self.test_mask = mask(self.test_pos, self.test_neg, dtype=bool)
-        self.train_data = to_tensor(self.train_pos)
-        self.test_data = to_tensor(self.test_pos)
+#         self.train_pos = self.sample(train_index)
+#         self.test_pos = self.sample(test_index)
+#         self.train_neg, self.test_neg = self.sample_negative()
+#         self.train_mask = mask(self.train_pos, self.train_neg, dtype=int)
+#         self.test_mask = mask(self.test_pos, self.test_neg, dtype=bool)
+#         self.train_data = to_tensor(self.train_pos)
+#         self.test_data = to_tensor(self.test_pos)
 
-    def set_seed(self):
-        np.random.seed(self.seed)  # NumPyのシードを設定
-        torch.manual_seed(self.seed)  # PyTorchのシードを設定
+#     def set_seed(self):
+#         np.random.seed(self.seed)  # NumPyのシードを設定
+#         torch.manual_seed(self.seed)  # PyTorchのシードを設定
 
-    def sample(self, index):
-        row = self.adj_mat.row
-        col = self.adj_mat.col
-        data = self.adj_mat.data
-        sample_row = row[index]
-        sample_col = col[index]
-        sample_data = data[index]
-        sample = sp.coo_matrix(
-            (sample_data, (sample_row, sample_col)), shape=self.adj_mat.shape
-        )
-        return sample
+#     def sample(self, index):
+#         row = self.adj_mat.row
+#         col = self.adj_mat.col
+#         data = self.adj_mat.data
+#         sample_row = row[index]
+#         sample_col = col[index]
+#         sample_data = data[index]
+#         sample = sp.coo_matrix(
+#             (sample_data, (sample_row, sample_col)), shape=self.adj_mat.shape
+#         )
+#         return sample
 
-    def sample_negative(self):
-        # identityは隣接行列が二部グラフかどうかを示す
-        # 二部グラフ：辺の両端の頂点が同じ頂点集合に属していないグラフ
-        pos_adj_mat = self.null_mask + self.adj_mat.toarray()
-        neg_adj_mat = sp.coo_matrix(np.abs(pos_adj_mat - np.array(1)))
-        all_row = neg_adj_mat.row
-        all_col = neg_adj_mat.col
-        all_data = neg_adj_mat.data
-        index = np.arange(all_data.shape[0])
+#     def sample_negative(self):
+#         # identityは隣接行列が二部グラフかどうかを示す
+#         # 二部グラフ：辺の両端の頂点が同じ頂点集合に属していないグラフ
+#         pos_adj_mat = self.null_mask + self.adj_mat.toarray()
+#         neg_adj_mat = sp.coo_matrix(np.abs(pos_adj_mat - np.array(1)))
+#         all_row = neg_adj_mat.row
+#         all_col = neg_adj_mat.col
+#         all_data = neg_adj_mat.data
+#         index = np.arange(all_data.shape[0])
 
-        # 負のテストセットをサンプリング
-        test_n = self.test_index.shape[0]
-        test_neg_index = np.random.choice(index, test_n, replace=False)
-        test_row = all_row[test_neg_index]
-        test_col = all_col[test_neg_index]
-        test_data = all_data[test_neg_index]
-        test = sp.coo_matrix(
-            (test_data, (test_row, test_col)), shape=self.adj_mat.shape
-        )
+#         # 負のテストセットをサンプリング
+#         test_n = self.test_index.shape[0]
+#         test_neg_index = np.random.choice(index, test_n, replace=False)
+#         test_row = all_row[test_neg_index]
+#         test_col = all_col[test_neg_index]
+#         test_data = all_data[test_neg_index]
+#         test = sp.coo_matrix(
+#             (test_data, (test_row, test_col)), shape=self.adj_mat.shape
+#         )
 
-        # 訓練セットをサンプリング
-        train_neg_index = np.delete(index, test_neg_index)
-        # train_n = self.train_index.shape[0]
-        # train_neg_index = np.random.choice(train_neg_index, train_n, replace=False)
-        train_row = all_row[train_neg_index]
-        train_col = all_col[train_neg_index]
-        train_data = all_data[train_neg_index]
-        train = sp.coo_matrix(
-            (train_data, (train_row, train_col)), shape=self.adj_mat.shape
-        )
-        return train, test
+#         # 訓練セットをサンプリング
+#         train_neg_index = np.delete(index, test_neg_index)
+#         # train_n = self.train_index.shape[0]
+#         # train_neg_index = np.random.choice(train_neg_index, train_n, replace=False)
+#         train_row = all_row[train_neg_index]
+#         train_col = all_col[train_neg_index]
+#         train_data = all_data[train_neg_index]
+#         train = sp.coo_matrix(
+#             (train_data, (train_row, train_col)), shape=self.adj_mat.shape
+#         )
+#         return train, test
 
-
-class NewSampler(object):
-    def __init__(self, original_adj_mat, null_mask, target_dim, target_index, seed):
-        super().__init__()
-        self.seed = seed
-        self.set_seed()
+class NewSampler:
+    def __init__(self, original_adj_mat, null_mask, target_dim, target_index):
         self.adj_mat = original_adj_mat
         self.null_mask = null_mask
         self.dim = target_dim
